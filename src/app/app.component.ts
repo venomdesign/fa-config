d3b5f4c1b5ec104dcebb739a54d8ebc873fb23ed
venomdesign <chris.schweigart@venomdesign.com>
2025-02-13 00:00:42 -0500

    Card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ExampleComponent } from './fa/fa.component';
 import { ButtonsComponent } from './components/buttons/buttons.component';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule, ExampleComponent, ButtonsComponent],
+  imports: [RouterOutlet, FontAwesomeModule, ExampleComponent, ButtonsComponent, CardComponent],
   templateUrl: './app.component.html', // External template
   styleUrls: ['./app.component.css'], // External styles (optional)
 })
---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card',
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+
+}
