4b836cb5d853128a18bd3945dadc56a1dddbbf1e
venomdesign <chris.schweigart@venomdesign.com>
2025-02-13 23:54:53 -0500

    Add tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,13 @@ import { HeaderComponent } from "./components/layout/header/header.component";
 import { ContentComponent } from "./components/layout/content/content.component";
 import { FooterComponent } from "./components/layout/footer/footer.component";
 import { PageHeaderComponent } from './components/layout/page/page-header/page-header.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { TabComponent } from './components/tabs/tab/tab.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FontAwesomeModule, ExampleComponent, ButtonsComponent, CardComponent, AccordionComponent, SidenavComponent, HeaderComponent, ContentComponent, FooterComponent, PageHeaderComponent],
+  imports: [RouterOutlet, FontAwesomeModule, ExampleComponent, ButtonsComponent, CardComponent, AccordionComponent, SidenavComponent, HeaderComponent, ContentComponent, FooterComponent, PageHeaderComponent, TabsComponent, TabComponent],
   templateUrl: './app.component.html', // External template
   styleUrls: ['./app.component.css'], // External styles (optional)
 })
--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { TabComponent } from './tab/tab.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabs',
-  imports: [],
+  imports: [TabComponent, FontAwesomeModule, CommonModule],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.scss'
 })
-export class TabsComponent {
 
+export class TabsComponent implements AfterContentInit {
+  @ContentChildren(TabComponent) tabs!: QueryList<TabComponent>;
+  
+  ngAfterContentInit() {
+    const activeTabs = this.tabs.filter(tab => tab.active);
+    if (activeTabs.length === 0) {
+      this.selectTab(this.tabs.first);
+    }
+  }
+  
+  selectTab(tab: TabComponent) {
+    this.tabs.forEach(t => t.active = false);
+    tab.active = true;
+  }
 }
